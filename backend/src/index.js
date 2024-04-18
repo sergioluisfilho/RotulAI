7a78a2231f7e3fb670f1746fba837e151f92857f
Sérgio Luís de Azevedo Cruz Filho <54141596+sergioluisfilho@users.noreply.github.com>
2024-04-18 11:22:55 -0300

    Initial Structure

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+// import routes from "./routes.js";
+// import pool from "./pool.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+// app.use(routes);
+
+app.listen(3000);
+console.log("Servidor iniciou");
+
+// Example query
+// pool.query("SELECT NOW()", (err, res) => {
+//   if (err) {
+//     console.error("Error executing query", err);
+//   } else {
+//     console.log("Query result:", res.rows);
+//   }
+// });
